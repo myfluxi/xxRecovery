56fba5731039efee2a00d7d82129e778bea49da9
myfluxi <mvdw@arcor.de>
2012-03-15 16:39:27 +0100

    Remove wrap count altogether

--- recovery.c
+++ recovery.c
@@ -494,22 +494,6 @@ get_menu_selection(char** headers, char** items, int menu_only,
         } else if (!menu_only) {
             chosen_item = action;
         }
-
-        
-        if (abs(selected - old_selected) > 1) {
-            wrap_count++;
-            if (wrap_count == 300) {
-                wrap_count = 0;
-                if (ui_get_showing_back_button()) {
-                    ui_print("Back menu button disabled.\n");
-                    ui_set_showing_back_button(0);
-                }
-                else {
-                    ui_print("Back menu button enabled.\n");
-                    ui_set_showing_back_button(1);
-                }
-            }
-        }
     }
 
     ui_end_menu();
